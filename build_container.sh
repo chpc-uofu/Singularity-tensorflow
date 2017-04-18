db619bced406ef3c947435df8b66eca099a85c1b
Martin Cuma <martin.cuma@utah.edu>
2017-04-18 11:36:04 -0600

    fix build

--- build_container.sh
+++ build_container.sh
@@ -1,3 +1,3 @@
 rm -f ubuntu_tensorflow_gpu.img
 sudo singularity create --size 8192 ubuntu_tensorflow_gpu.img
-sudo singularity bootstrap ubuntu_tensorflow_gpu.img Singularity.def
+sudo singularity bootstrap ubuntu_tensorflow_gpu.img Singularity
